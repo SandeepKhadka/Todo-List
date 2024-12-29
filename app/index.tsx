3ba83836736094c0d6e69e967cc66806a582e25b
SandeepKhadka <87227387+SandeepKhadka@users.noreply.github.com>
2024-12-29 15:12:41 +0545

    todo card

--- app/index.tsx
+++ app/index.tsx
@@ -3,8 +3,9 @@ import React from "react";
 import { ThemedView } from "@/components/ThemedView";
 import { ThemedText } from "@/components/ThemedText";
 import { useFonts } from "expo-font";
+import TaskCard from "@/components/TaskCard";
 const Todo = () => {
-  const [loaded, error]= useFonts({
+  const [fontsLoaded] = useFonts({
     "Poppins-SemiBold": require("../assets/fonts/Poppins SemiBold.ttf"),
   });
   return (
@@ -13,6 +14,12 @@ const Todo = () => {
       <ThemedView style={styles.bottomContainer}>
         <ThemedView style={styles.topText}>
           <ThemedText style={styles.greet}>Good Afternoon</ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.task}>
+          <ThemedText style={{ fontFamily: "Poppins-SemiBold", fontSize: 14, marginBottom: 20 }}>
+            Task list
+          </ThemedText>
+          <TaskCard />
         </ThemedView>
       </ThemedView>
     </ThemedView>
@@ -23,19 +30,22 @@ export default Todo;
 
 const styles = StyleSheet.create({
   upperContainer: {
-    flex: 0.3,
+    flex: 0.4,
     backgroundColor: "#50C2C9",
   },
-  bottomContainer: { flex: 1 },
+  bottomContainer: { flex: 1, marginRight: 20, marginLeft: 20 },
   topText: {
-    flex: 0.4,
+    flex: 0.3,
     display: "flex",
     flexDirection: "row",
     justifyContent: "flex-end",
-    padding: 10,
   },
   greet: {
-    fontFamily: "Poppins SemiBold",
-    fontSize: 20,
+    fontFamily: "Poppins-SemiBold",
+    fontSize: 12,
+    marginTop: 15,
+  },
+  task: {
+    flex: 0.65,
   },
 });
