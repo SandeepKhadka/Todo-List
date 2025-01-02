21bab0860e4ed13b513f4ee9e68caeff9ab5fa7a
SandeepKhadka <87227387+SandeepKhadka@users.noreply.github.com>
2025-01-02 12:29:56 +0545

    task list

--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -1,15 +1,65 @@
 import { FontAwesome } from "@expo/vector-icons";
 import { ThemedText } from "./ThemedText";
 import { ThemedView } from "./ThemedView";
-import { StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import Checkbox from "expo-checkbox";
+import { useState } from "react";
+import { useFonts } from "expo-font";
 
 export default function TaskCard() {
+  const [isChecked, setChecked] = useState(false);
+  const [fontsLoaded] = useFonts({
+    "Poppins-SemiBold": require("../assets/fonts/Poppins SemiBold.ttf"),
+  });
+  const taskData = [
+    "Wake up at 7am",
+    "Learn DSA by 9am",
+    "Do the chores by 12pm",
+    "Start coding after 12pm",
+    "Do Coding upto 5pm",
+    "Eat meal for gym by 6pm",
+    "Go to gym and return by 8pm",
+    "Do chores by 9pm",
+    "Learn new topics by 10pm",
+    "Learn Book by 11pm",
+    "Go to Sleep after 11pm",
+  ];
   return (
     <View style={styles.taskCard}>
       <View style={styles.taskHeader}>
-        <ThemedText style={{color: '#000000'}}>Daily Task</ThemedText>
+        <ThemedText
+          style={{
+            color: "#000000",
+            fontFamily: "Poppins-SemiBold",
+            fontSize: 12,
+          }}
+        >
+          Daily Task
+        </ThemedText>
         <FontAwesome size={25} name="plus" color={"#50C2C9"} />
       </View>
+      <FlatList
+        data={taskData}
+        keyExtractor={(item, index) => String(index)}
+        renderItem={({ item }) => (
+          <View style={styles.taskLists}>
+            <Checkbox
+              value={isChecked}
+              onValueChange={setChecked}
+              color={isChecked ? "#4630EB" : undefined}
+            />
+            <Text
+              style={{
+                marginLeft: 10,
+                fontFamily: "Poppins-SemiBold",
+                fontSize: 12,
+              }}
+            >
+              {item}
+            </Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -25,8 +75,13 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
   taskHeader: {
-    flex: 1,
+    marginBottom: 10,
     justifyContent: "space-between",
     flexDirection: "row",
+  },
+  taskLists: {
+    flexDirection: "row",
+    width: "100%",
+    margin: 8,
   },
 });
